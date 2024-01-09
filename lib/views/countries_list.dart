04fea442a3100fbb09fa48ec3835e5093cbed353
saylee21 <sayleebharsakle@gmail.com>
2024-01-09 17:55:19 +0530

    errors resolved

--- lib/views/countries_list.dart
+++ lib/views/countries_list.dart
@@ -11,65 +11,104 @@ class CountriesListScreen extends StatefulWidget {
 }
 
 class _CountriesListScreenState extends State<CountriesListScreen> {
-
   TextEditingController searchController = TextEditingController();
+
+  late final StatsServices statsServices = StatsServices();
+
   @override
   Widget build(BuildContext context) {
-    StatsServices statsServices = StatsServices();
+    // StatsServices statsServices = StatsServices();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
       body: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: (value){
-                    setState(() {
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onChanged: (value){
+                      setState(() {
 
-                    });
-                  },
-                  controller: searchController,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                    hintText: 'Search country name',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50.0)
-                    )
+                      });
+                    },
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      hintText: 'Search country name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0)
+                      )
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                  child: FutureBuilder(
-                      future: statsServices.countiesListApi(),
-                      builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
-                        if(!snapshot.hasData){
-                          return ListView.builder(
-                              itemCount: 6,
-                              itemBuilder: (context, index){
-                                return Shimmer.fromColors(
-                                    baseColor: Colors.grey.shade700,
-                                    highlightColor: Colors.grey.shade100,
-                                    child: Column(
-                                      children: [
-                                        ListTile(
-                                          title: Container(height: 10, width: 80, color: Colors.white,),
-                                          subtitle: Container(height: 10, width: 80, color: Colors.white,),
-                                          leading: Container(height: 50, width: 50, color: Colors.white,),
-                                        )
-                                      ],
-                                    )
-                                );
-                              });
-                        }else{
-                          return ListView.builder(
-                            itemCount: snapshot.data!.length,
-                              itemBuilder: (context, index){
-                                String name = snapshot.data![index]['country'];
-                                if(searchController.text.isEmpty) {
+                FutureBuilder(
+                    future: statsServices.countiesListApi(),
+                    builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+                      if(!snapshot.hasData){
+                        return ListView.builder(
+                          shrinkWrap: true,
+                            itemCount: 6,
+                            itemBuilder: (context, index){
+                              return Shimmer.fromColors(
+                                  baseColor: Colors.grey.shade700,
+                                  highlightColor: Colors.grey.shade100,
+                                  child: Column(
+                                    children: [
+                                      ListTile(
+                                        title: Container(height: 10, width: 80, color: Colors.white,),
+                                        subtitle: Container(height: 10, width: 80, color: Colors.white,),
+                                        leading: Container(height: 50, width: 50, color: Colors.white,),
+                                      )
+                                    ],
+                                  )
+                              );
+                            });
+                      }else{
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index){
+                              String name = snapshot.data![index]['country'];
+                              if(searchController.text.isEmpty) {
+                              return Column(
+                                children: [
+                                  InkWell(
+                                    onTap: (){
+                                      Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailScreen(
+                                        name: snapshot.data![index]['country'],
+                                        todayRecovered: snapshot.data![index]['todayRecovered'],
+                                        critical: snapshot.data![index]['critical'],
+                                        active: snapshot.data![index]['active'],
+                                        image: snapshot.data![index]['countryInfo']['flag'],
+                                        test: snapshot.data![index]['tests'],
+                                        totalCases: snapshot.data![index]['cases'],
+                                        totalDeaths: snapshot.data![index]['deaths'],
+                                        totalRecovered: snapshot.data![index]['recovered'],
+                                      )));
+                                    },
+                                    child: ListTile(
+                                      title: Text(
+                                          snapshot.data![index]['country']),
+                                      subtitle: Text(
+                                          snapshot.data![index]['cases']
+                                              .toString()),
+                                      leading: Image(
+                                        height: 50,
+                                        width: 50,
+                                        image: NetworkImage(
+                                            snapshot
+                                                .data![index]['countryInfo']['flag']
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              );
+                            }else if(name.toLowerCase().contains(searchController.text.toLowerCase())){
                                 return Column(
                                   children: [
                                     InkWell(
@@ -104,50 +143,15 @@ class _CountriesListScreenState extends State<CountriesListScreen> {
                                     )
                                   ],
                                 );
-                              }else if(name.toLowerCase().contains(searchController.text.toLowerCase())){
-                                  return Column(
-                                    children: [
-                                      InkWell(
-                                        onTap: (){
-                                          Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailScreen(
-                                            name: snapshot.data![index]['country'],
-                                            todayRecovered: snapshot.data![index]['todayRecovered'],
-                                            critical: snapshot.data![index]['critical'],
-                                            active: snapshot.data![index]['active'],
-                                            image: snapshot.data![index]['countryInfo']['flag'],
-                                            test: snapshot.data![index]['tests'],
-                                            totalCases: snapshot.data![index]['cases'],
-                                            totalDeaths: snapshot.data![index]['deaths'],
-                                            totalRecovered: snapshot.data![index]['recovered'],
-                                          )));
-                                        },
-                                        child: ListTile(
-                                          title: Text(
-                                              snapshot.data![index]['country']),
-                                          subtitle: Text(
-                                              snapshot.data![index]['cases']
-                                                  .toString()),
-                                          leading: Image(
-                                            height: 50,
-                                            width: 50,
-                                            image: NetworkImage(
-                                                snapshot
-                                                    .data![index]['countryInfo']['flag']
-                                            ),
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  );
-                              }else{
-                                  return Container();
-                              }
-                              });
-                        }
+                            }else{
+                                return Container();
+                            }
+                            });
                       }
-                  )
-              )
-            ],
+                    }
+                )
+              ],
+            ),
           ),
       ),
     );
--- lib/views/world_stats.dart
+++ lib/views/world_stats.dart
@@ -6,6 +6,7 @@ import 'package:pie_chart/pie_chart.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../widgets/reusable_row.dart';
 
+
 class WorldStatsScreen extends StatefulWidget {
   const WorldStatsScreen({super.key});
 
@@ -13,17 +14,11 @@ class WorldStatsScreen extends StatefulWidget {
   State<WorldStatsScreen> createState() => _WorldStatsScreenState();
 }
 
-class _WorldStatsScreenState extends State<WorldStatsScreen> with TickerProviderStateMixin{
-
+class _WorldStatsScreenState extends State<WorldStatsScreen> with TickerProviderStateMixin {
   late final AnimationController _controller = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this)..repeat();
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat();
 
   final List<Color> colorList = [
     const Color(0xff4285F4),
@@ -31,93 +26,101 @@ class _WorldStatsScreenState extends State<WorldStatsScreen> with TickerProvider
     const Color(0xffde5246),
   ];
 
+  late final StatsServices statsServices = StatsServices();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    StatsServices statsServices = StatsServices();
     return Scaffold(
-      body: SafeArea(child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height:MediaQuery.of(context).size.height *0.01,),
-              FutureBuilder(future:statsServices.fetchWorldStatsRecords() ,
-                  builder: (context, AsyncSnapshot<WorldStatsModel> snapshot){
-                if(!snapshot.hasData){
-                  return Expanded(
-                      flex: 1,
-                      child: SpinKitFadingCircle(
-                        color: Colors.white,
-                        size: 50,
-                        controller: _controller,
-                      )
-                  );
-                }else{
-                  return Column(
-                    children: [
-                      PieChart(
-                        dataMap:  {
-                          "Total": double.parse(snapshot.data!.cases!.toString()),
-                          "Recovered": double.parse(snapshot.data!.recovered!.toString()),
-                          "Deaths": double.parse(snapshot.data!.deaths!.toString()),
-                        },
-                        chartValuesOptions: const ChartValuesOptions(
-                          showChartValuesInPercentage: true
-                        ),
-                        animationDuration:const Duration(milliseconds: 1200),
-                        chartType: ChartType.ring,
-                        colorList: colorList,
-                        legendOptions: const LegendOptions(
-                            legendPosition: LegendPosition.left
-                        ),
-                        chartRadius: MediaQuery.of(context).size.width/3.2,
-                      ),
-          
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.06),
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 15, right: 15,top: 10, bottom: 10),
-                            child: Column(
-                              children: [
-                                ReusableRow(title: 'Total', value: snapshot.data!.cases.toString()),
-                                ReusableRow(title: 'Deaths', value: snapshot.data!.deaths.toString()),
-                                ReusableRow(title: 'Recovered', value: snapshot.data!.recovered.toString()),
-                                ReusableRow(title: 'Active', value: snapshot.data!.active.toString()),
-                                ReusableRow(title: 'Critical', value: snapshot.data!.critical.toString()),
-                                ReusableRow(title: 'Today Deaths', value: snapshot.data!.todayDeaths.toString()),
-                                ReusableRow(title: 'Today Recovered', value: snapshot.data!.todayRecovered.toString()),
-                              ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+                FutureBuilder(
+                    future: statsServices.fetchWorldStatsRecords(),
+                    builder: (context, AsyncSnapshot<WorldStatsModel> snapshot) {
+                      if (!snapshot.hasData) {
+                        return SpinKitFadingCircle(
+                          color: Colors.white,
+                          size: 50,
+                          controller: _controller,
+                        );
+                      } else {
+                        return Column(
+                          children: [
+                            PieChart(
+                              dataMap: {
+                                "Total": double.parse(snapshot.data!.cases!.toString()),
+                                "Recovered": double.parse(snapshot.data!.recovered!.toString()),
+                                "Deaths": double.parse(snapshot.data!.deaths!.toString()),
+                              },
+                              chartValuesOptions: const ChartValuesOptions(
+                                showChartValuesInPercentage: true,
+                              ),
+                              animationDuration: const Duration(milliseconds: 1200),
+                              chartType: ChartType.ring,
+                              colorList: colorList,
+                              legendOptions: const LegendOptions(
+                                legendPosition: LegendPosition.left,
+                              ),
+                              chartRadius: MediaQuery.of(context).size.width / 3.2,
                             ),
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=> const CountriesListScreen()));
-                        },
-                        child: Container(
-                          height: 50,
-                          decoration:  BoxDecoration(
-                              color: const Color(0xff1aa260),
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: const Center(
-                            child: Text('Track Countries',
-                             style: TextStyle(
-                               fontSize: 20
-                             ),),
-                          ),
-                        ),
-                      )
-                    ],
-                  );
-                }
-              }),
-            ],
+
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.06),
+                              child: Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+                                  child: Column(
+                                    children: [
+                                      ReusableRow(title: 'Total', value: snapshot.data!.cases.toString()),
+                                      ReusableRow(title: 'Deaths', value: snapshot.data!.deaths.toString()),
+                                      ReusableRow(title: 'Recovered', value: snapshot.data!.recovered.toString()),
+                                      ReusableRow(title: 'Active', value: snapshot.data!.active.toString()),
+                                      ReusableRow(title: 'Critical', value: snapshot.data!.critical.toString()),
+                                      ReusableRow(title: 'Today Deaths', value: snapshot.data!.todayDeaths.toString()),
+                                      ReusableRow(title: 'Today Recovered', value: snapshot.data!.todayRecovered.toString()),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => const CountriesListScreen()));
+                              },
+                              child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff1aa260),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    'Track Countries',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        );
+                      }
+                    }),
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
